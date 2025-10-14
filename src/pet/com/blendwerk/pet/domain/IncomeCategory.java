e7488cfd083cb27700e52c4d75531abeeb712637
Fernando Arturo Gómez Flores <fernando.gomez@blendwerk.com.mx>
2025-10-14 16:15:38 -0600

    feat: agregar enumeración ExpenseCategory y IncomeCategory para clasificar gastos e ingresos

---
+++ src/pet/com/blendwerk/pet/domain/IncomeCategory.java
@@ -0,0 +1,20 @@
+package com.blendwerk.pet.domain;
+
+public enum IncomeCategory {
+    SALARY,
+    BUSINESS,
+    INVESTMENTS,
+    RENTALS,
+    ROYALTIES,
+    FREELANCE,
+    CONSULTING,
+    DIVIDENDS,
+    INTEREST,
+    CAPITAL_GAINS,
+    PENSION,
+    SOCIAL_SECURITY,
+    ALIMONY,
+    CHILD_SUPPORT,
+    GOVERNMENT_BENEFITS,
+    OTHER
+}
