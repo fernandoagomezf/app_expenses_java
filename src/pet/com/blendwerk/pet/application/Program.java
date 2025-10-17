14a540e371b770974a446a37b434e86ce1c159cb
Fernando Arturo Gómez Flores <fernando.gomez@blendwerk.com.mx>
2025-10-17 12:55:40 -0600

    feat: adición de clase Program

---
+++ src/pet/com/blendwerk/pet/application/Program.java
@@ -0,0 +1,9 @@
+package com.blendwerk.pet.application;
+
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("Hello, PET!");
+        var i = 42;
+        System.out.println(i);
+    }
+}
--- src/pet/module-info.java
+++ src/pet/module-info.java
@@ -2,5 +2,6 @@
 module pet {
     exports com.blendwerk.pet.domain;
     exports com.blendwerk.pet.infrastructure;
+    exports com.blendwerk.pet.application;
     requires java.base;
 }
